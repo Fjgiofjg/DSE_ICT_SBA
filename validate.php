3dd5beed196c5a7cd000691c53a5a016346589e4
Fjgiofjg <126320863+Fjgiofjg@users.noreply.github.com>
2024-09-11 16:13:42 +0800

    Update validate.php
    added reset pw check

--- validate.php
+++ validate.php
@@ -1,41 +1,43 @@
 <?php
-  $uid =$_POST['uid'];
-  $password =$_POST['pw'];
-  $checked=0;
-  $server = "localhost";
-  $username = "Stellar_Database";
-  $DBpassword = "pwdxvbKL2YKyn6Ca";
-  $database = "stellar_database";
-      
-  $link = mysqli_connect($server, $username, $DBpassword, $database);
-    if (!$link){
-      die("Something went wrong");
+$uid = $_POST['uid'];
+$password = $_POST['pw'];
+$checked = 0;
+$server = "localhost";
+$username = "Stellar_Database";
+$DBpassword = "pwdxvbKL2YKyn6Ca";
+$database = "stellar_database";
+
+$link = mysqli_connect($server, $username, $DBpassword, $database);
+if (!$link) {
+    die("Something went wrong");
+}
+
+// Fetch data from users table
+$query = "SELECT * FROM users WHERE uid='$uid'";
+$result = mysqli_query($link, $query);
+
+if ($row = mysqli_fetch_assoc($result)) {
+    // Check if the user has a password reset request
+    $requestQuery = "SELECT * FROM password_resets WHERE email='{$row['uid']}'";
+    $requestResult = mysqli_query($link, $requestQuery);
+
+    if (mysqli_num_rows($requestResult) > 0) {
+        echo "<script>";
+        echo "alert('A password reset has been requested for this account. Please complete the process before logging in.');";
+        echo "window.location.href='login.html';";
+        echo "</script>";
+        exit();
     }
-    else{
-      echo "<script>console.log('DB link successful!')</script>";
+
+    // Verify password
+    if (password_verify($password, $row['Password'])) { // Use password_verify for hashed passwords
+        // Success
+        setcookie("uid", $uid, time() + 3600);
+        echo "<script>window.location.href='home.php';</script>";
+    } else {
+        echo "<script>alert('Invalid User ID / Password!'); window.location.href='login.html';</script>";
     }
-
-  //Fetch data
-	$query = "SELECT * FROM users WHERE uid='".$uid."' AND Password='".$password."'";
-	$result = mysqli_query($link, $query);
-
-  while ($row = mysqli_fetch_assoc($result)){
-    //echo "Username:".$row["Username"].",uid:".$row["uid"].",Password:".$row["Password"].",Is_Admin:".$row["Is_Admin"].",Class:".$row["Class"].",Class_No:".$row["Class_No"]."<br>";
-    $checked = 1;
-  }
-
-  //Compair user input and DB data
-  if ($checked==1){
-    //success
-    setcookie("uid",$uid,time()+3600);
-    echo "<script>";
-    echo "window.location.href='home.php'";
-    echo "</script>";
-  }
-  else{
-    echo "<script>";
-    echo "alert('Invalid User ID / Password!');";
-    echo "window.location.href='login.html'";
-    echo "</script>";
-  }
+} else {
+    echo "<script>alert('Invalid User ID / Password!'); window.location.href='login.html';</script>";
+}
 ?>
